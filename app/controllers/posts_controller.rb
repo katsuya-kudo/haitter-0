b3d20bdf304bee475a556d91695b1b9e9f0ef32e
EC2 Default User <ec2-user@ip-172-31-26-67.ap-northeast-1.compute.internal>
2022-10-27 09:08:15 +0000

    second commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,39 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
   end
-
-  def show
+  
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
   end
 
+  def show
+    @post = Post.find_by(id: params[:id])
+    puts @post.content
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+  
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
+  
   def api
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
-  get 'posts/show'=> 'posts#show'
+  post 'posts/create' => 'posts#create'
+  get 'posts/:id'=> 'posts#show'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
   get 'posts/api' => 'posts#api'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'home#top'
