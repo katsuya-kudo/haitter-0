37529cbcc3f2f5fae2b659ecb29dedd6c321a424
EC2 Default User <ec2-user@ip-172-31-26-67.ap-northeast-1.compute.internal>
2022-11-10 06:25:11 +0000

    6回目のコミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,12 +40,6 @@ class UsersController < ApplicationController
     @user.user_name = params[:name]
     @user.email = params[:email]
     
-    if params[:image]
-      @user.image_name = "#{@user.id}.jpg"
-      image = params[:imagge]
-      File.binwrite("public/user_umages/#{@user.image_name}",image.read)
-    end
-    
     if @user.save
       flash[:notice] = "ユーザー情報を編集しました"
       redirect_to("/users/#{@user.id}")
